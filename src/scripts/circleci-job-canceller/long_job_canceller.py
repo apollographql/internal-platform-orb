02e2b7048c57fb551983c21f22538f2ad6b28b79
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-07-18 13:12:33 -0400

    CI setup and relatively full list of orb commands (#1)
    * orb basics, and CI
    
    * shorten line
    
    * include tests
    
    * just don't do tests
    
    * don't require it
    
    * hmm try this
    
    * does need this one
    
    * name
    
    * more intro words
    
    * add style guide
    
    * remove boilerplate
    
    * do not use this - deprecated - orb
    
    * update URLs
    
    * test setup command to download source
    
    So we don't have to package scripts into the YAML file
    
    * use correct parameter
    
    * commit YAMLlint config file
    
    * save artifacts to help debugging
    
    * merge circleci-job-canceller into this orb
    
    * update per style guide
    
    * lowercase p
    
    * move this script here too
    
    * move render-yaml-from-tsv into own folder
    
    so can have own requirements.txt
    
    * verbose mode
    
    * extract this script from closed source
    
    * add type
    
    * requires continuation orb
    
    * include some alert-slack commands
    
    * update docs
    
    * add gcp-authorize command
    
    * delete boilerplate / example stuff
    
    * remove bash unit testing for deleted greeting.sh
    
    * don't reference now deleted job
    
    * don't target a specific commit, that was for testing something
    
    * updated documentation
    
    * use correct keys
    
    * add github-auth command
    
    * add proceed-if-changes command
    
    * brought over some (most?) github related commands
    
    * add a default that matches our internal oft-used name for this
    
    * add alert-slack job
    
    * rename this command to be more obvious why it exists
    
    * update callsite
    
    * update callsite
    
    * namespaces are the SaSS we interact with
    
    * update style guide
    
    * fix name
    
    * new optional parameter: delete bot comments
    
    * right formatting now??
    
    * add new option to really delete comments
    
    * fix bash issue
    
    * nope, this
    
    * correct if
    
    * this way instead
    
    * allow GCP service accounts to be activated too, as an auth option
    
    * couple more commands extracted from common use
    
    * allow file to not be deleted (later processes may need it)
    
    * name
    
    * user helper
    
    * use printf - avoid adding a newline to an empty env value
    
    * slightly nicer message
    
    * rename per Sass prefix convention
    
    * extract tflint script we have
    
    * try this, make shellcheck happy
    
    * avoid hard coded paths
    
    * try this instead
    
    thanks Alpine shell...
    
    * add new commands from next targec
    
    * mispelled the first name
    
    * update docs
    
    * new _job_ for this
    
    * delete sample boilerplate
    
    * fix link
    
    * allow people to opt out of terragrunt during tg-tf-check-formatting
    
    * better documentation

---
+++ src/scripts/circleci-job-canceller/long_job_canceller.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import requests
+import datetime
+import sys
+import os.path
+
+from dateutil.parser import *
+
+import argparse
+import pprint
+
+sys.path.append("..") # added!
+from Modules.circle_utils import *
+
+standard_headers = {}
+
+
+job_life_clock = datetime.timedelta(hours=2)
+job_midlife_warning = datetime.timedelta(hours=1)
+
+
+def get_workflow_started_by(current_workflow):
+    user_url = f"https://circleci.com/api/v2/user/{current_workflow['started_by']}"
+    user_info = requests.get(user_url, headers=standard_headers).json()
+    username = user_info.get("login", "")  # the Github / CircleCI scheduling bot won't have a username (JSON body will be {'message': 'Not found.'})
+
+    return username
+
+
+def find_old_workflow_ids(repo_slug):
+    utc_tz = datetime.timezone(datetime.timedelta(hours=0))
+    now = datetime.datetime.now(utc_tz)
+
+    for current_pipeline in get_all_items(f"/project/gh/{repo_slug}/pipeline", standard_headers):
+      for current_workflow in get_all_items(f"/pipeline/{current_pipeline['id']}/workflow", standard_headers):
+          if current_workflow["status"] == "on_hold":
+              created_at_str = current_workflow["created_at"]
+              created_at = isoparse(created_at_str)
+
+              if ( (created_at < (now - job_midlife_warning)) and (created_at > (now - job_life_clock))):
+                  username = get_workflow_started_by(current_workflow)
+                  yield { "job_status": "age_warning", "name": current_workflow['name'], "id": current_workflow['id'], "username": username }
+
+              if (created_at < (now - job_life_clock)):
+                  username = get_workflow_started_by(current_workflow)
+                  yield { "job_status": "too_old", "name": current_workflow['name'], "id": current_workflow['id'], "username": username }
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("circleapitoken", help="the CircleCI API token for this script")
+    parser.add_argument("orgreposlug", help="the location, user-or-org/repository-name , of this repository")
+
+    parser.add_argument("--output-file", help="output to file path", default="/tmp/notifications.tsv", )
+    parser.add_argument("--just-do-it", help="just cancel jobs", default=False, action="store_true")
+
+    args = parser.parse_args()
+
+    CIRCLE_API_KEY = args.circleapitoken
+    cancel_jobs_here = args.just_do_it
+
+    standard_headers = {"Circle-Token": args.circleapitoken}
+
+    simple_path = os.path.abspath(os.path.expanduser(os.path.expandvars(args.output_file)))
+    with open(simple_path, 'w') as f:
+        f.write("job_status\tproceed\tid\tusername\tname\n")
+        for current_info in find_old_workflow_ids(args.orgreposlug):
+            if current_info["job_status"] == "age_warning":
+                print(f"midlife warning for workflow ({current_info['name']}), started by gh:{current_info['username']}. See more info at: https://app.circleci.com/pipelines/workflows/{current_info['id']}")
+            else:
+                print(f"found too old workflow: {current_info['id']} ({current_info['name']}) See more info at: https://app.circleci.com/pipelines/workflows/{current_info['id']}")
+                if cancel_jobs_here:
+                    requests.post(f"https://circleci.com/api/v2/workflow/{current_info['id']}/cancel", headers=standard_headers)
+
+            f.write(f"{current_info['job_status']}\ttrue\t{current_info['id']}\t{current_info['username']}\t{current_info['name']}\n")
---
+++ src/scripts/circleci-job-canceller/main_is_borked.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import requests
+
+import argparse
+import pprint
+import json
+import http
+import datetime
+import sys
+from dateutil.parser import *
+
+# Turn on / off debugging
+#http.client.HTTPConnection.debuglevel = 1
+
+
+oncall_subteam = "<!subteam^SFNQZTE4Q>"
+
+leave_pending_jobs_for = datetime.timedelta(hours=1)
+
+def make_graphql_query(githubtoken, orgreposlug):
+
+    org_repo = orgreposlug.split("/")
+    [org_name, repo_name] = org_repo
+
+    # reduce dependencies by doing the GraphQL part myself
+    # Play with this in Apollo Studio Explorer: https://tinyurl.com/4v7nsdmk
+    graph_ql_query = {"query": """query($org_name: String!, $repo_name: String!) {
+  viewer {
+    organization(login: $org_name) {
+      repository(name: $repo_name) {
+        object(expression: "main") {
+          ... on Commit {
+            commitUrl
+            status {
+              state
+              contexts {
+                targetUrl
+                createdAt
+                state
+                context
+              }
+            }
+            author {
+             user {
+               login
+             }
+            }
+          }
+        }
+      }
+    }
+  }
+} """, "variables": {"org_name": org_name, "repo_name": repo_name} }
+
+# potentially other interesting snippets
+#            associatedPullRequests(first: 5) {
+#              edges {
+#                node {
+#                  url      <-- returns the URL of the PR(s) associated with this commit
+#                }
+#              }
+#            }
+
+    graph_ql_verify = {"query": """query { viewer { login } }"""}
+
+    res = requests.post("https://api.github.com/graphql", json=graph_ql_query, headers={
+        "Authorization": f"Bearer {githubtoken}",
+        "Content-Type": "application/json"
+    })
+
+    return res
+
+
+def main(githubtoken, orgreposlug):
+    now = datetime.datetime.now( datetime.timezone(datetime.timedelta(hours=0)) ) #datetime in UTC
+
+    res = make_graphql_query(githubtoken, orgreposlug)
+
+    commit_info = None
+
+    if res.json().get("errors"):
+        messages = []
+        [messages.append(x.get('message')) for x in res.json().get("errors")]
+        print(f"{oncall_subteam} an error was reported by the scheduled main commit checker. Message was {','.join(messages)}")
+        sys.exit(1)
+
+    try:
+        commit_info = res.json().get("data").get("viewer").get("organization").get("repository").get("object")
+    except AttributeError as e:
+        print(e)
+        pprint.pprint(res.json())
+        raise e
+
+    commit_state = commit_info.get("status").get("state")
+
+    commit_url = commit_info.get("commitUrl")
+    author = commit_info.get("author").get("user").get("login")
+
+    if (commit_state == "PENDING"):
+        # This is a slightly more targetted nudge than what log_job_canceller does as it's ONLY for head of main
+        # (but _technically_ duplicated reminders)
+        #
+        # regardless main's HEAD commit spends most of its time here, experimentally
+        pending_workflows = []
+        [ pending_workflows.append(x) for x in commit_info.get("status").get("contexts") if ( ((x.get("state") == "PENDING") and ("ci/circleci" in x.get("context"))) and not("dev0" in x.get("context")))  ]
+        # pending workflows targetting dev0 don't deserve a callout by themselves
+
+        if ( len(pending_workflows) > 0 ):
+            jobs_started_str = pending_workflows[0].get("createdAt")
+            workflow_link = pending_workflows[0].get("targetUrl")   # will always be the same targetURL
+
+            jobs_stated_dt = isoparse(jobs_started_str)
+
+            if ( jobs_stated_dt < ( now - leave_pending_jobs_for ) ):
+                print(f"Hey *gh:{author}*! Looks like your merge has spent some time in _staging_ to be tested. Go to *prod* :question: :link: <{workflow_link}|View on CircleCI>. - :heart: :canned_food:.")
+
+    if (commit_state == "FAILURE"):
+        print(f"Recent checks have found monorepo main is broken. Latest commit {commit_url} by gh:{author}. {oncall_subteam}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("githubapitoken", help="the Github API token for this script")
+    # Token needs to have the following scopes:
+    #   * admin:org read:org
+    #   * repo repo:status
+
+    parser.add_argument("orgreposlug", help="the location, user-or-org/repository-name , of this repository")
+
+    args = parser.parse_args()
+
+    main(args.githubapitoken, args.orgreposlug)
