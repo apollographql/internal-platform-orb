17c059e037086ce131392cc404d80f130a71c28d
AlexanderMann <mannimal09@gmail.com>
2023-01-19 14:10:53 -0600

    Fix: Move headers to be arg based, more functional paradigm

--- src/scripts/circleci-job-canceller/long_job_canceller.py
+++ src/scripts/circleci-job-canceller/long_job_canceller.py
@@ -12,8 +12,6 @@ import argparse
 
 sys.path.append("..")  # added!
 
-standard_headers = {}
-
 
 job_life_clock = datetime.timedelta(hours=2)
 job_midlife_warning = datetime.timedelta(hours=1)
@@ -21,34 +19,36 @@ job_midlife_warning = datetime.timedelta(hours=1)
 robot_committers = ["apollo-bot2"]
 
 
-def get_workflow_started_by(current_workflow):
+def get_workflow_started_by(current_workflow, headers):
     user_url = f"https://circleci.com/api/v2/user/{current_workflow['started_by']}"
-    user_info = requests.get(user_url, headers=standard_headers).json()
+    user_info = requests.get(user_url, headers=headers).json()
     # the Github / CircleCI scheduling bot won't have a username (JSON body will be {'message': 'Not found.'})
     username = user_info.get("login", "")
 
     return username
 
 
-def find_old_workflow_ids(repo_slug):
+def find_old_workflow_ids(repo_slug, headers):
     utc_tz = datetime.timezone(datetime.timedelta(hours=0))
     now = datetime.datetime.now(utc_tz)
 
-    for current_pipeline in get_all_items(f"/project/gh/{repo_slug}/pipeline", standard_headers):
-        for current_workflow in get_all_items(f"/pipeline/{current_pipeline['id']}/workflow", standard_headers):
+    for current_pipeline in get_all_items(f"/project/gh/{repo_slug}/pipeline", headers):
+        for current_workflow in get_all_items(f"/pipeline/{current_pipeline['id']}/workflow", headers):
             if current_workflow["status"] == "on_hold":
                 created_at_str = current_workflow["created_at"]
                 created_at = isoparse(created_at_str)
 
                 if ((created_at < (now - job_midlife_warning)) and (created_at > (now - job_life_clock))):
-                    username = get_workflow_started_by(current_workflow)
+                    username = get_workflow_started_by(
+                        current_workflow, headers)
                     if username in robot_committers:
                         continue
 
                     yield {"job_status": "age_warning", "name": current_workflow['name'], "id": current_workflow['id'], "username": username}
 
                 if (created_at < (now - job_life_clock)):
-                    username = get_workflow_started_by(current_workflow)
+                    username = get_workflow_started_by(
+                        current_workflow, headers)
                     yield {"job_status": "too_old", "name": current_workflow['name'], "id": current_workflow['id'], "username": username}
 
 
@@ -73,9 +73,10 @@ if __name__ == "__main__":
 
     simple_path = os.path.abspath(os.path.expanduser(
         os.path.expandvars(args.output_file)))
+
     with open(simple_path, 'w') as f:
         f.write("job_status\tproceed\tid\tusername\tname\n")
-        for current_info in find_old_workflow_ids(args.orgreposlug, ):
+        for current_info in find_old_workflow_ids(args.orgreposlug, standard_headers):
             if current_info["job_status"] == "age_warning":
                 print(
                     f"midlife warning for workflow ({current_info['name']}), started by gh:{current_info['username']}. See more info at: https://app.circleci.com/pipelines/workflows/{current_info['id']}")
