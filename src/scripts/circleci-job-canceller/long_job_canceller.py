9d721a3cc927a005a00198d282c6b672e08d345c
AlexanderMann <mannimal09@gmail.com>
2023-10-17 15:34:08 -0400

    Refactor: Slim down what the cancel command and script are supposed to do

--- src/scripts/circleci-job-canceller/long_job_canceller.py
+++ src/scripts/circleci-job-canceller/long_job_canceller.py
@@ -2,9 +2,7 @@
 
 from Modules.circle_utils import *
 import datetime
-import itertools
 import os.path
-import pprint
 import requests
 import sys
 
@@ -15,9 +13,6 @@ import argparse
 
 sys.path.append("..")  # added!
 
-
-job_life_clock = datetime.timedelta(hours=2)
-job_midlife_warning = datetime.timedelta(hours=1)
 utc_tz = datetime.timezone(datetime.timedelta(hours=0))
 now = datetime.datetime.now(utc_tz)
 
@@ -63,10 +58,9 @@ def pipeline_created_at_to_datetime(pipeline):
 def find_old_workflow_ids(
         repo_slug,
         window_start,
-        window_end_cancel,
-        window_end_warn,
+        window_end,
         headers):
-    print("find_old_workflow_ids({repo_slug}, start={window_start}, cancel_before={window_end_cancel}, warn_before={window_end_warn})")
+    print("find_old_workflow_ids({repo_slug}, window_start={window_start}, window_end={window_end})")
 
     pipelines_walked = -1
 
@@ -78,7 +72,7 @@ def find_old_workflow_ids(
         if pipelines_walked % 100 == 0:
             print(f"find_old_workflow_ids(...): at {created_at} / {window_start}")
 
-        if window_end_warn < created_at:
+        if window_end < created_at:
             print(f'find_old_workflow_ids(...): Before window end, pipeline too young: {created_at}')
             continue
         if created_at < window_start:
@@ -86,28 +80,16 @@ def find_old_workflow_ids(
             return None
 
         for current_workflow in get_all_items(f"/pipeline/{current_pipeline['id']}/workflow", headers, None):
-            job_status = None
-            username = get_workflow_started_by(current_workflow, headers)
-            logging_detail = f'[{created_at}] ({current_workflow["name"]})[{current_workflow["status"]}], started by gh:{username}. See more info at: https://app.circleci.com/pipelines/workflows/{current_workflow["id"]}'
+            if not current_workflow.get('stopped_at') and created_at < window_end:
+                username = get_workflow_started_by(current_workflow, headers)
 
-            if not current_workflow.get('stopped_at'):
-                if (created_at < window_end_cancel):
-                    print(f'find_old_workflow_ids(...): found too old workflow {logging_detail}')
-                    job_status = "too_old"
+                print(
+                    f'find_old_workflow_ids(...): found too old workflow [{created_at}] ({current_workflow["name"]})[{current_workflow["status"]}], started by gh:{username}. See more info at: https://app.circleci.com/pipelines/workflows/{current_workflow["id"]}')
 
-                elif username in robot_committers:
-                    continue
-
-                elif current_workflow['status'] == 'on_hold':
-                    print(f'find_old_workflow_ids(...): midlife warning for workflow {logging_detail}')
-                    job_status = "age_warning"
-
-            if job_status:
                 yield {
-                    "job_status": job_status,
-                    "name": current_workflow['name'],
                     "id": current_workflow['id'],
-                    "username": username
+                    "username": username,
+                    "name": current_workflow['name']
                 }
 
 
@@ -129,51 +111,41 @@ def has_only_ignored_jobs(current_info, ignore, headers):
 
 def main(
     circleapitoken, orgreposlug,
-    n_windows,
     window_start_in_hours,
-    window_cancel_end_in_hours,
-    window_warning_end_in_hours,
+    window_end_in_hours,
     output_file, commit, ignore
 ):
     standard_headers = {"Circle-Token": circleapitoken}
 
     simple_path = os.path.abspath(os.path.expanduser(
         os.path.expandvars(output_file)))
-    
-    window_start = now - (job_life_clock * n_windows)
-    if window_start_in_hours:
-        window_start = now - datetime.timedelta(hours=window_start_in_hours)
 
-    window_end_cancel = now - job_life_clock
-    if window_cancel_end_in_hours:
-        window_end_cancel = now - datetime.timedelta(hours=window_cancel_end_in_hours)
-
-    window_end_warn = now - job_midlife_warning
-    if window_warning_end_in_hours:
-        window_end_warn = now - datetime.timedelta(hours=window_warning_end_in_hours)
+    window_start = now - datetime.timedelta(hours=window_start_in_hours)
+    window_end = now - datetime.timedelta(hours=window_end_in_hours)
 
     with open(simple_path, 'w') as f:
-        f.write("job_status\tproceed\tid\tusername\tname\n")
+        f.write("orgreposlug\tid\tusername\tname\n")
         for current_info in find_old_workflow_ids(
             orgreposlug,
             window_start,
-            window_end_cancel,
-            window_end_warn,
+            window_end,
             standard_headers
         ):
-            if current_info["job_status"] == "age_warning":
-                if not (ignore == ['']):
-                    if has_only_ignored_jobs(current_info, ignore, standard_headers):
-                        print(
-                            f"main(...): ignoring workflow: {current_info['id']} See more info at https://app.circleci.com/pipelines/workflows/{current_info['id']}")
-                        continue
-            else:
-                if commit:
-                    http_post(
-                        f"https://circleci.com/api/v2/workflow/{current_info['id']}/cancel", headers=standard_headers)
+            if commit:
+                print(f'main(...): cancelling {current_info["id"]}')
+                http_post(
+                    f"https://circleci.com/api/v2/workflow/{current_info['id']}/cancel", headers=standard_headers)
+                print(f'main(...): cancelled {current_info["id"]}')
+            elif current_info['username'] in robot_committers:
+                print(f'main(...): ignoring robot_committer {current_info["username"]} workflow: {current_info["id"]}')
+                continue
+            elif not ignore == [''] and has_only_ignored_jobs(current_info, ignore, standard_headers):
+                print(
+                    f"main(...): ignoring workflow: {current_info['id']} See more info at https://app.circleci.com/pipelines/workflows/{current_info['id']}")
+                continue
 
             f.write(
-                f"{current_info['job_status']}\ttrue\t{current_info['id']}\t{current_info['username']}\t{current_info['name']}\n")
+                f"{orgreposlug}\t{current_info['id']}\t{current_info['username']}\t{current_info['name']}\n")
 
 
 if __name__ == "__main__":
@@ -190,29 +162,21 @@ if __name__ == "__main__":
                         help="just cancel jobs",
                         default=False,
                         action="store_true")
-    parser.add_argument("--n-windows",
-                        help="Number of windows to look back across. Default window length is 2 hours.",
-                        type=int,
-                        default=6)
     parser.add_argument("--window-start-in-hours",
                         help="Number of hours ago to start search window. Default usage is the n-windows argument.",
                         type=int,
                         default=0)
-    parser.add_argument("--window-end-cancel-in-hours",
+    parser.add_argument("--window-end-in-hours",
                         help="Number of hours ago to end search window for cancels. ie, if the Workflows found in the search window, are last updated _before_ this watermark, cancel them. Default usage is the n-windows argument.",
-                        type=int,
-                        default=0)
-    parser.add_argument("--window-end-warning-in-hours",
-                        help="Number of hours ago to end search window for warnings. Assumes this is less than window-end-cancel-in-hours when present. Default usage is the n-windows argument.",
                         type=int,
                         default=0)
     parser.add_argument("--ignore-job-names", help="if all awaiting approval jobs in the pipeline contain this word, do not age warn about it. Multiple word supported by delimiting with , (example: --ignore=optional,maybe). A job only has to match one of the words", default="")
 
     args = parser.parse_args()
 
+    print(args.commit)
+
     main(args.circleapitoken, args.orgreposlug,
-         args.n_windows,
          args.window_start_in_hours,
-         args.window_end_cancel_in_hours,
-         args.window_end_warning_in_hours,
+         args.window_end_in_hours,
          args.output_file, args.commit, args.ignore_job_names.split(","))
